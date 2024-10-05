a31c289e6a86a698f0499742886e54dabb498674
Mohamed Uvais <uvais@cloudframe.com>
2024-10-05 11:58:45 +0400

    presetup for project5

---
+++ project5/project5.py
@@ -0,0 +1,20 @@
+
+"""
+Project 5
+    Store extracted questions in mysql
+Requirements
+    Update project 4 and add support for database
+    Create a database to store the following
+        Subject Name
+        Question Text
+        Answer options
+        Chapter name
+    Load a PDF containing questions
+    Extract each question as per a regular expression
+    Store each question in the database
+Error Handling
+    Take care of case where database is not available
+    Take care of case where table is not available
+    Take care of any error handling in DB operations
+
+"""
